3bdeb0257e60691d470b2d8533f4c0c1dbb8e97c
meminewa <suphawat_j@kkumail.com>
2022-09-24 23:02:47 +0700

    - [feat] complete hello world

--- hello_test.go
+++ hello_test.go
@@ -2,10 +2,21 @@ package main
 
 import "testing"
 
-func TestHelloX(t *testing.T) {
-	got := Hello("x")
-	want := "Hello, x"
+func TestHello(t *testing.T) {
+	t.Run("say hello to people", func(t *testing.T) {
+		got := Hello("x")
+		want := "Hello, x"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
 
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
